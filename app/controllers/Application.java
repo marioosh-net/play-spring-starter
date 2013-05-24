73621d4cbc1eb5560fb7a389c7823004e10d3ef6
marioosh <marioosh.net@gmail.com>
2013-05-24 15:03:35 +0200

    using Akka and asynchronous results

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import java.util.List;
+import java.util.concurrent.Callable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import play.data.Form;
+import play.libs.Akka;
+import play.libs.F.Function;
 import play.mvc.Controller;
 import play.mvc.Result;
 import services.HelloService;
@@ -14,19 +19,33 @@ public class Application extends Controller {
 
 	@Autowired
 	private HelloService helloService;
-	
+
 	@Autowired
 	private UserDAO userDAO;
-	
-  	public Result index() {
-    	return ok(index.render(helloService.hello(), userDAO.find()));
-  	}
 
-  	public Result add() {
-  		Form<User> userForm = Form.form(User.class);
-  		userForm = userForm.bindFromRequest();
-  		userDAO.add(userForm.get());
-  		return redirect("/");
-  	}
-  	
+	public Result index() {
+		
+		return async(Akka.future(new Callable<Object>() {
+
+			@Override
+			public Object call() throws Exception {
+				return new Object[] {helloService.hello(), userDAO.find()};
+			}
+		}).map(new Function<Object, Result>() {
+
+			@Override
+			public Result apply(Object a) throws Throwable {
+				Object[] b = (Object[]) a;
+				return ok(index.render((String)b[0], (List<User>)b[1]));
+			}
+		}));
+	}
+
+	public Result add() {
+		Form<User> userForm = Form.form(User.class);
+		userForm = userForm.bindFromRequest();
+		userDAO.add(userForm.get());
+		return redirect("/");
+	}
+
 }
